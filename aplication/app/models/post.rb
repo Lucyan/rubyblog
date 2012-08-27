bfc8f86dd86ca2fe26c39d34ff9eb4c3bb7f94df
Social Feedback <Social Feedback@SocialFeedback>
2012-08-27 17:02:27 -0400

    Validaciones

--- aplication/app/models/post.rb
+++ aplication/app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :title
+  
+  validates :title, :content, :presence => true
+  validates :title, :length => { :minimum => 2 }
+  validates :title, :uniqueness => { :message => "El título ya está en uso" }
 end

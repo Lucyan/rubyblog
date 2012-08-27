a1065acd556a27af8f7177e687ad35c866219ae1
Social Feedback <Social Feedback@SocialFeedback>
2012-08-27 12:22:14 -0400

    Primer commit

---
+++ aplication/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyblog::Application.config.secret_token = '06827adfe179215f3c5e38635de7ebc7d590584712eeb3ed44b3191668ba9d9a62cdb7c24f45ffbfa5e878e7bd167ee76f5915cbaa1134b4ffa26440007fa79a'
